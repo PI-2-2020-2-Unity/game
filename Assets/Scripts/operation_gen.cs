6169405d28fd0d869d7bba23771e3319bf3bf2f5
Mario Chincha <mario.chincha@utec.edu.pe>
2020-10-09 13:40:59 -0500

    prueba3

--- Assets/Scripts/operation_gen.cs
+++ Assets/Scripts/operation_gen.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class operation_gen : MonoBehaviour
 {
-    private int val1;
+    public static int val1 = 0;
     public static int n1;
     public static int n2;
-    public static int true_N1;
-    public static int true_N2;
-    public static string operador;
+    public static List<int> true_N1 = new List<int>();
+    public static List<int> true_N2 = new List<int>();
+    public static List<string> operador = new List<string>();
     private int operacion;
-    Dictionary<int, int> lista_de_valores;
+    public static List<int> respuesta = new List<int>();
     // Start is called before the first frame update
     void Start()
     {
@@ -21,81 +21,86 @@ public class operation_gen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad8))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            val1 = 1;
-            operator_gen(val1);
+            if (val1 == 3)
+            {
+                val1 = 0;
+            }
+            else
+            {
+                val1++;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Keypad6))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            val1 = 2;
-            operator_gen(val1);
+            if (val1 == 0)
+            {
+                val1 = 3;
+            }
+            else
+            {
+                val1--;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-        {
-            val1 = 3;
-            operator_gen(val1);
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            val1 = 4;
-            operator_gen(val1);
-        }
-        val1 = 0;
-
     }
     public void operator_gen(int x)
     {
         n1 = Random.Range(1, 10);
         n2 = Random.Range(1, 10);
-        switch (x){
+        switch (x)
+        {
             case 1:
                 Debug.Log("Suma");
-                operacion = n1+n2;
-                true_N1 = n1;
-                true_N2 = n2;
-                operador = "+";
+                operacion = n1 + n2;
+                true_N1.Add(n1);
+                true_N2.Add(n2);
+                operador.Add("+");
                 Debug.Log(n1 + "+" + n2 + "=" + operacion);
-                
+                respuesta.Add(operacion);
+
                 break;
             case 2:
                 Debug.Log("Resta");
                 if (n1 >= n2)
                 {
                     operacion = n1 - n2;
-                    true_N1 = n1;
-                    true_N2 = n2;
+                    true_N1.Add(n1);
+                    true_N2.Add(n2);
                     Debug.Log(n1 + "-" + n2 + "=" + operacion);
+
                 }
                 else
                 {
                     operacion = n2 - n1;
-                    true_N1 = n2;
-                    true_N2 = n1;
+                    true_N1.Add(n2);
+                    true_N2.Add(n1);
                     Debug.Log(n2 + "-" + n1 + "=" + operacion);
                 }
-                operador = "-";
+                respuesta.Add(operacion);
+                operador.Add("-");
                 break;
             case 3:
                 Debug.Log("Multiplicacion");
                 operacion = n1 * n2;
-                operador = "×";
-                true_N1 = n1;
-                true_N2 = n2;
+                operador.Add("×");
+                true_N1.Add(n1);
+                true_N2.Add(n2);
                 Debug.Log(n1 + "x" + n2 + "=" + operacion);
-                
+                respuesta.Add(operacion);
                 break;
             case 4:
                 Debug.Log("Division");
                 operacion = n1 * n2;
-                true_N1 = operacion;
-                true_N2 = n1;
-                operador = "÷";
+                true_N1.Add(operacion);
+                true_N2.Add(n1);
+                operador.Add("÷");
                 Debug.Log(operacion + "÷" + n1 + "=" + n2);
-                
+                respuesta.Add(n2);
                 break;
             default:
                 break;
+
         }
     }
 }

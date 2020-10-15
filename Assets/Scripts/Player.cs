4542d4aaecf2892156eb9244c8b558ff26fb62f0
otreblan <otreblain@gmail.com>
2020-10-15 16:53:37 -0500

    Make the player look at the pointer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,36 +2,47 @@
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody rb;
+    Rigidbody2D rb;
     public float forwardForce = 500f;
     public GameObject bullet;
 
+    public Transform pointer;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void FixedUpdate()
     {
-        if (Input.GetKey("d"))
-        {
-            // transform.Translate(Vector3.forward * Time.deltaTime);
-            rb.AddForce(forwardForce * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-forwardForce * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
-        }
+        Vector2 dist = pointer.position - transform.position;
 
-        if (Input.GetKey("space"))
-        {
-            GameObject bulletObject = Instantiate(bullet);
-            bulletObject.transform.position = transform.position + transform.forward;
-            bulletObject.transform.forward = transform.forward;
-        }
+        float angle = Mathf.Atan2(dist.y, dist.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+
+        //if (Input.GetKey("d"))
+        //{
+        //    // transform.Translate(Vector3.forward * Time.deltaTime);
+        //    rb.AddForce(forwardForce * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKey("a"))
+        //{
+        //    rb.AddForce(-forwardForce * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKey("w"))
+        //{
+        //    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        //}
+        //if (Input.GetKey("s"))
+        //{
+        //    rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        //}
+
+        //if (Input.GetKey("space"))
+        //{
+        //    GameObject bulletObject = Instantiate(bullet);
+        //    bulletObject.transform.position = transform.position + transform.forward;
+        //    bulletObject.transform.forward = transform.forward;
+        //}
 
     }
 }

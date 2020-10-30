5e739f5c5cc716d54e7086f65366a377222df9a1
luisejv <luis.jauregui@utec.edu.pe>
2020-10-30 13:33:22 -0500

    space shooter package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMax, xMin, zMax, zMin;
+
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 5f, moveVertical);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 0f, Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+        rb.rotation = Quaternion.Euler(0f, 0f, -rb.velocity.x * tilt);
+    }
+}

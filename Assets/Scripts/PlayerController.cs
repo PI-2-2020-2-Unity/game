dc5ace9f92083644516f0aa95223b19c436a3e2b
luisejv <luis.jauregui@utec.edu.pe>
2020-10-30 14:12:49 -0500

    made lights prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 5f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
         rb.velocity = movement * speed;
         rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 0f, Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
         rb.rotation = Quaternion.Euler(0f, 0f, -rb.velocity.x * tilt);

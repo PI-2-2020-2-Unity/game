202625eaaea975b310f7bb2c56e96ef8a528393a
otreblan <otreblain@gmail.com>
2020-11-19 18:57:48 -0500

    Make each tile the same size as the sprite

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -8,17 +8,28 @@ public class Background : MonoBehaviour
     public Transform player;
     public Tile tile;
     Tilemap tilemap;
+    Grid grid;
 
     // Start is called before the first frame update
     void Start()
     {
         tilemap = GetComponentInChildren<Tilemap>();
+        grid = GetComponent<Grid>();
+
+        Bounds spriteBox = tile.sprite.bounds;
+
+        Debug.Log(tile.sprite.bounds);
+
+        grid.cellSize = spriteBox.size;
+
+        tilemap.SetTile(Vector3Int.zero, tile);
     }
 
     // Update is called once per frame
     void Update()
     {
         // TODO Add and remove tiles when the player moves
+        //tilemap.SetTile(Vector3Int.FloorToInt(player.position), tile);
     }
 }
 

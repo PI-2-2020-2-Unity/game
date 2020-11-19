a38e9093c04d73b22a23a7e58e6e54b204b05060
otreblan <otreblain@gmail.com>
2020-11-19 18:26:24 -0500

    Add Background.cs

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Background : MonoBehaviour
+{
+    public Transform player;
+    public Tile tile;
+    Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO Add and remove tiles when the player moves
+    }
+}
+
+// vim: set expandtab:

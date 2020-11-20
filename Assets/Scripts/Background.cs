2cc4d897164ff05b464c6f3f248b1c7074a38f28
otreblan <otreblain@gmail.com>
2020-11-19 19:07:29 -0500

    Add tiles

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -10,26 +10,31 @@ public class Background : MonoBehaviour
     Tilemap tilemap;
     Grid grid;
 
+    Vector3Int WorldToGrid(Vector3 position)
+    {
+        Vector3Int res = tilemap.WorldToCell(position);
+
+        res.z = 0;
+
+        return res;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         tilemap = GetComponentInChildren<Tilemap>();
         grid = GetComponent<Grid>();
 
-        Bounds spriteBox = tile.sprite.bounds;
-
-        Debug.Log(tile.sprite.bounds);
-
-        grid.cellSize = spriteBox.size;
-
-        tilemap.SetTile(Vector3Int.zero, tile);
+        grid.cellSize = tile.sprite.bounds.size;
+        //tilemap.SetTile(Vector3Int.zero, tile);
     }
 
     // Update is called once per frame
     void Update()
     {
         // TODO Add and remove tiles when the player moves
-        //tilemap.SetTile(Vector3Int.FloorToInt(player.position), tile);
+        if(player)
+            tilemap.SetTile(WorldToGrid(player.position), tile);
     }
 }
 
